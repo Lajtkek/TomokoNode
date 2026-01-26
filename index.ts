3b8297a5dacf4b9a12d603e8243a4d142a2b2f82
Matěj Lajtkep <Lajtkep@xdent.cz>
2026-01-26 07:49:52 +0100

    add first discord integreation

--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
-const a = "Hello world!"
+import { Client, GatewayIntentBits, Events } from "discord.js";
 
-console.log(a);
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+dotenv.config({ path: ".env.local", override: true });
+
+console.log(process.env.TOKEN )
+const token = process.env.TOKEN ?? (() => {
+  throw new Error("TOKEN environment variable is not set");
+})();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
