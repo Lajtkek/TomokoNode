885ae8040b9071023eece20d07c34d9c04a62345
Matěj Lajtkep <Lajtkep@xdent.cz>
2026-01-28 19:43:34 +0100

    v1 of test prisma db model

--- modules/reactCounter.ts
+++ modules/reactCounter.ts
@@ -3,7 +3,8 @@ import { Client, GatewayIntentBits, Events } from "discord.js";
 export function addReactionCountModule(client: Client){
 
     client.on(Events.MessageReactionAdd, (reaction, user) => {
-        console.log(`${user.username} reacted ${reaction.emoji} na "${reaction.message.content}"`)
+        console.log("n")
+        console.log(reaction.emoji)
     })
 
     client.on(Events.MessageCreate, (message) => {
